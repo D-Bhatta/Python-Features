e2da2ac82abb7c4901b6c65355d984a95843ccf7
D-Bhatta <dbhatta1232@gmail.com>
2020-08-14 13:15:02 -0400

    Add template files for setup
    The following files were generated from templates:
    
    pre-commit-config.yaml:
      - Precommit config file
    
    travis.yml:
      - Travis-CI config file
    
    notes/Notes.md:
      - Markdown notes file
    
    notes/Notes_Python_Features.docx:
      - MS Word notes file
    
    requirements_dev.txt:
      - Python environment requirements file
    
    scripts/scripts.py:
      - Template python file
    
    tests/test_project-name.py:
      - Template tests file

---
+++ notes/Notes.md
@@ -0,0 +1,25 @@
+# Python-Features
+
+Notes and code about python features
+
+## Table of Contents
+
+.
+
+## Sections
+
+.
+
+## Notes
+
+1. .
+
+## Project Status
+
+.
+
+## Additional Information
+
+### Screenshots
+
+### Links
---
+++ tests/test_project-name.py
@@ -0,0 +1,17 @@
+""" Tests for 'scripts' package """
+import pytest
+
+from ..scripts import scripts
+
+
+def test_helloworld(capsys):
+    """ Correct object argument prints """
+    scripts.helloworld("cat")
+    captured = capsys.readouterr()
+    assert "cat" in captured.out
+
+
+# This is supposed to fail
+def test_helloworld_exception():
+    with pytest.raises(TypeError):
+        scripts.helloworld("1")
